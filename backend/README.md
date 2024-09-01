ac12f67fa9a20ff0117e3612447efbf1d62db33a
Elgyuoshy <hamogh@gmail.com>
2024-09-01 13:26:52 -0700

    first commit

---
+++ backend/README.md
@@ -0,0 +1,4 @@
+to run Server by nodemon  => npm run server
+to run Server by node  => npm run start
+to install => npm i
+ 
---
+++ frontend/vite.config.js
@@ -0,0 +1,37 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    strategies: 'injectManifest',
+    srcDir: 'src',
+    filename: 'sw.js',
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'frontend',
+      short_name: 'frontend',
+      description: 'AuraMoms is a website aimed at moms who want to find tech-assisted solutions for self-care and productivity. It will offer a curated selection of gadgets and tools to help moms relax, recharge, and manage their busy schedules. The website will initially target European and US markets and will be optimized for mobile devices.',
+      theme_color: '#ffffff',
+    },
+
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+    },
+
+    devOptions: {
+      enabled: true,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
